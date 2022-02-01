fec6c5bdb2d75856036b6cc4cf3217961868f90b
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-01-31 21:33:15 -0700

    commit tree 7a2dd7fe1e823e1787d45dfe017878372d26e106

---
+++ src/components/Band.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Band extends Component {
+    
+    handleOnClick = () => {
+        this.props.deleteBand(this.props.id)
+    };
+
+    render () {
+        return (
+            <div>
+                <li>
+                    <span>{this.props.name}</span>
+                    <button onClick={this.handleOnClick}>DELETE</button>
+                </li>
+            </div>
+        )
+    }
+}
+
+export default Band
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -2,8 +2,18 @@ export default function manageBand(state = {
   bands: [],
 }, action) {
   switch(action.type) {
+
     case "ADD_BAND":
+      const band = {
+        id: Math.random(),
+        name: action.name
+      }
       return {...state, bands: state.bands.concat(action.name)}
+
+    case "DELETE_BAND":
+      const bands = state.bands.filter(band => band.id !== action.id)
+      return {bands}
+
     default:
       return state
   
